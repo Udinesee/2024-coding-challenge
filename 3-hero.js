730dbf1a98790f1119bb5a91f3c0f9ac02a4a231
Udinesee <akhmadkhoirudin85@gmail.com>
2024-01-29 14:49:45 +0700

    day 3/366

---
+++ 3-hero.js
@@ -0,0 +1,23 @@
+// // Deenqto Coding Challenge 2024
+// 2/366
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+// function hero(bullets, dragons) {
+//     if (bullets / 2 >= dragons) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// function hero(bullets, dragons) {
+//     return (bullets / 2 >= dragons) ? true : false;
+// }
+
+function hero(bullets, dragons) {
+    return (bullets >= dragons * 2) ? true : false;
+}
+
+// const hero = (bullets, dragons) => bullets >= dragons * 2;
+
+console.log(hero(10, 5), true);
